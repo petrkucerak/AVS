edeb0b91e7b5b96c5b0b5f42ff6940f082c8fb0b
petrkucerak <petr@khome.cz>
2024-04-12 14:00:29 +0200

    Config I2C

--- labs/07/PROJECTS/ACCELEARAMETR/src/main.c
+++ labs/07/PROJECTS/ACCELEARAMETR/src/main.c
@@ -1,5 +1,6 @@
 #include "stm32f10x.h"
 #include "stm32f10x_gpio.h"
+#include "stm32f10x_i2c.h"
 #include "stm32f10x_rcc.h"
 #include "stm32vldiscovery.h"
 
@@ -11,6 +12,7 @@ int main(void)
    /* Structure declaration */
    GPIO_InitTypeDef GPIOB_InitStructure;
    GPIO_InitTypeDef GPIOC_InitStructure;
+   I2C_InitTypeDef I2C_InitStructure;
 
    /* Enable clocks */
    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
@@ -31,6 +33,18 @@ int main(void)
    GPIOC_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
    GPIOC_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
    GPIO_Init(GPIOC, &GPIOC_InitStructure);
+
+   /* Init I2C communication */
+   I2C_StructInit(&I2C_InitStructure);
+   I2C_InitStructure.I2C_ClockSpeed = 100000; // 100kHz
+   I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
+   I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
+   I2C_InitStructure.I2C_OwnAddress1 =
+       0x00; // device is in mater mode, specification is not required
+   I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
+   I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+   I2C_StructInit(&I2C_InitStructure);
+   I2C_Init(I2C1, &I2C_InitStructure);
 
    while (1) {
       GPIO_SetBits(GPIOC, GPIO_Pin_8);
