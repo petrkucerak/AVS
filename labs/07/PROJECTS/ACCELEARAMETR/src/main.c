35b58723fe66fa58e0ddc92f4ec693de0507d7ec
petrkucerak <petr@khome.cz>
2024-04-12 10:45:43 +0200

    Config peripherals

--- labs/07/PROJECTS/ACCELEARAMETR/src/main.c
+++ labs/07/PROJECTS/ACCELEARAMETR/src/main.c
@@ -9,23 +9,36 @@ int main(void)
 {
 
    /* Structure declaration */
-   GPIO_InitTypeDef GPIO_InitStructure;
-   GPIO_StructInit(&GPIO_InitStructure);
+   GPIO_InitTypeDef GPIOB_InitStructure;
+   GPIO_InitTypeDef GPIOC_InitStructure;
 
-   /* enable IO port C */
-   // RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+   /* Enable clocks */
    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE)
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 
-   // PC6 SR_Dat, PC7 SR_Ow, PC8 SR_Clk, PC9 LD3 [Green]
-   GPIO_InitStructure.GPIO_Pin =
-       GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9;
-   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
-   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-   GPIO_Init(GPIOC, &GPIO_InitStructure);
+   /* Define B peripherals */
+   /* PB9 (I2C1 SDA) a PB8 (I2C1 CLK) */
+   GPIO_StructInit(&GPIOB_InitStructure);
+   GPIOB_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_8;
+   GPIOB_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+   GPIOB_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+   GPIO_Init(GPIOB, &GPIOB_InitStructure);
+
+   /* Define C peripherals */
+   /* PC8 LD4 [Blue], PC9 LD3 [Green] */
+   GPIO_StructInit(&GPIOC_InitStructure);
+   GPIOC_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+   GPIOC_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+   GPIOC_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+   GPIO_Init(GPIOC, &GPIOC_InitStructure);
 
    while (1) {
-      delay(500000);
+      GPIO_SetBits(GPIOC, GPIO_Pin_8);
+      GPIO_ResetBits(GPIOC, GPIO_Pin_9);
+      delay(5000000);
+      GPIO_ResetBits(GPIOC, GPIO_Pin_8);
+      GPIO_SetBits(GPIOC, GPIO_Pin_9);
+      delay(5000000);
    }
 }
 
