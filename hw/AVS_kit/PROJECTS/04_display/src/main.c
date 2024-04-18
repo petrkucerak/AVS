9b3ee0c670bfcf80016e0b5031e89478ab43149a
petrkucerak <petr@khome.cz>
2024-04-18 16:24:31 +0200

    Config SPI

--- hw/AVS_kit/PROJECTS/04_display/src/main.c
+++ hw/AVS_kit/PROJECTS/04_display/src/main.c
@@ -1,7 +1,7 @@
 #include "stm32f10x.h"
 #include "stm32f10x_gpio.h"
-#include "stm32f10x_i2c.h"
 #include "stm32f10x_rcc.h"
+#include "stm32f10x_spi.h"
 #include "stm32vldiscovery.h"
 
 void delay(vu32 nCount);
@@ -10,21 +10,23 @@ int main(void)
 {
 
    /* Structure declaration */
-   GPIO_InitTypeDef GPIOB_InitStructure;
+   GPIO_InitTypeDef GPIOA_InitStructure;
    GPIO_InitTypeDef GPIOC_InitStructure;
-   I2C_InitTypeDef I2C_InitStructure;
+   SPI_InitTypeDef SPI_InitStructure;
 
    /* Enable clocks */
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
 
-   /* Define B peripherals */
-   /* PB9 (I2C1 SDA) a PB8 (I2C1 CLK) */
-   GPIO_StructInit(&GPIOB_InitStructure);
-   GPIOB_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_8;
-   GPIOB_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
-   GPIOB_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
-   GPIO_Init(GPIOB, &GPIOB_InitStructure);
+   /* Define A peripherals */
+   /* PA4 [SPI1_NSS], PA5 [SPI1_SCK], PA6 [SPI1_MISO], PA7 [SPI1_MOSI] */
+   GPIO_StructInit(&GPIOA_InitStructure);
+   GPIOA_InitStructure.GPIO_Pin =
+       GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
+   GPIOA_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+   GPIOA_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+   GPIO_Init(GPIOA, &GPIOA_InitStructure);
 
    /* Define C peripherals */
    /* PC8 LD4 [Blue], PC9 LD3 [Green] */
@@ -34,25 +36,29 @@ int main(void)
    GPIOC_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
    GPIO_Init(GPIOC, &GPIOC_InitStructure);
 
-   /* Init I2C communication */
-   I2C_StructInit(&I2C_InitStructure);
-   I2C_InitStructure.I2C_ClockSpeed = 100000; // 100kHz
-   I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
-   I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
-   I2C_InitStructure.I2C_OwnAddress1 =
-       0x00; // device is in mater mode, specification is not required
-   I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
-   I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-   I2C_StructInit(&I2C_InitStructure);
-   I2C_Init(I2C1, &I2C_InitStructure);
+   /* SPI 1 configuration */
+   SPI_StructInit(&SPI_InitStructure);
+   SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+   SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+   SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+   SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
+   SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
+   SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+   SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16;
+   SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+   SPI_InitStructure.SPI_CRCPolynomial = 7;
+   SPI_Init(SPI1, &SPI_InitStructure);
+
+   /* Enable the SPI1  */
+   SPI_Cmd(SPI1, ENABLE);
 
    while (1) {
       GPIO_SetBits(GPIOC, GPIO_Pin_8);
       GPIO_ResetBits(GPIOC, GPIO_Pin_9);
-      delay(4000000);
+      delay(1000000);
       GPIO_ResetBits(GPIOC, GPIO_Pin_8);
       GPIO_SetBits(GPIOC, GPIO_Pin_9);
-      delay(4000000);
+      delay(1000000);
    }
 }
 
